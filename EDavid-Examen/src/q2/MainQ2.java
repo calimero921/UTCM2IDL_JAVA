dff90565f6d061def18c14b8ee5fb091e067f1d8
bede6362 <bede6362@EB-OR1009018.ad.francetelecom.fr>
2013-11-08 12:09:42 +0100

    Mise à jour

--- EDavid-Examen/src/q2/MainQ2.java
+++ EDavid-Examen/src/q2/MainQ2.java
@@ -1,10 +1,92 @@
 package q2;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class MainQ2 {
 
 	public static String DOC1 = "documents/q2doc1.json";
-	public static void main(String[] args) {
+	public static String DOC2 = "documents/q2doc2.json";
+	public static String DOC3 = "documents/q2doc3.json";
+	public static String DOC4 = "documents/q2doc4.json";
 
+	public static void main(String[] args) {
+		//Q2-1
+		Shape q2Obj1;
+		q2Obj1 = readJSONfile(DOC1);
+		System.out.println(q2Obj1.toString());
+		
+		Shape q2Obj2;
+		q2Obj2 = readJSONfile(DOC2);
+		System.out.println(q2Obj2.toString());
+
+		Shape q2Obj3;
+		q2Obj3 = readJSONfile(DOC3);
+		System.out.println(q2Obj3.toString());
+		
+		//Q2-2
+		Shape q2obj4=new Shape();
+		q2obj4.setNom("Octogone");
+		q2obj4.setNbCote(8);
+		saveJSONfile(DOC4,q2obj4);
+		System.out.println(q2Obj3.toString());
+		
+		//Q2-3
+		List<Shape> liste = new ArrayList<Shape>();
+		liste.add(new Shape("Triangle", 3));
+		liste.add(new Shape("Carre", 4));
+		liste.add(new Shape("pentagone", 5));
+		liste.add(new Shape("Hexagone", 6));
+
+		//Q2-4
+		//la clé représentant le nombre de coté sera stocké dans l'Integer de tête 
+		//tandis que les objets seront stockes dans la liste qui suit
+		TreeMap<Integer, ArrayList<Shape>> map = new TreeMap<Integer, ArrayList<Shape>>();
+	}
+	
+	public static Shape readJSONfile(String filename){	
+		Shape local= new Shape();
+		
+		//lit les éléments du fichier
+		JsonFactory j = new JsonFactory();
+		ObjectMapper mapper = new ObjectMapper();
+		JsonParser jp;
+
+		try {
+			File f = new File(filename);
+			local = mapper.readValue(f, Shape.class);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		return local;
+	}
+	public static void saveJSONfile(String filename, Shape local){
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			File f = new File(filename);
+			mapper.writeValue(f, local);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- EDavid-Examen/src/q2/Shape.java
+++ EDavid-Examen/src/q2/Shape.java
@@ -1,11 +1,33 @@
 package q2;
 
 public class Shape {
-  String nom;
-  Integer nbCote;
-public Shape() {
-  nom = "sans";
-  nbCote = -1;
-}
-  
+	String nom;
+	Integer nbCote;
+
+	public Shape() {
+		this.nom = "sans";
+		this.nbCote = -1;
+	}
+	public Shape(String nom, Integer nbCote) {
+		this.nom = nom;
+		this.nbCote = nbCote;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Integer getNbCote() {
+		return this.nbCote;
+	}
+	public void setNbCote(Integer nbCote) {
+		this.nbCote = nbCote;
+	}
+
+	public String toString() {
+		return "Shape : nom=" + this.nom + ", nbCote=" + this.nbCote;
+	}
 }
